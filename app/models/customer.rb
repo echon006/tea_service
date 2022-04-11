c8b9f6847b05389bf3d3030df91983171aa60e84
Eric Chon <echon92@gmail.com>
2022-04-11 14:13:15 -0600

    validations for customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,10 @@
 class Customer < ApplicationRecord
   has_many :subscriptions
 
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :email
+  validates_presence_of :address
+
+  validates_uniqueness_of :email
 end
