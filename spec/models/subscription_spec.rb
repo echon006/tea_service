a45c20da2d957f7970ed35a57b1f12c03a529f99
Eric Chon <echon92@gmail.com>
2022-04-11 14:19:04 -0600

    validations for subs

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -5,4 +5,13 @@ RSpec.describe Subscription, type: :model do
     it { should belong_to(:tea) }
     it { should belong_to(:customer) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:title)}
+    it { should validate_presence_of(:price)}
+    it { should validate_presence_of(:status)}
+    it { should validate_presence_of(:frequency)}
+    it { should validate_presence_of(:tea_id)}
+    it { should validate_presence_of(:customer_id)}
+  end
 end
